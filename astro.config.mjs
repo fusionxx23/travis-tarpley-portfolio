c58620423b1c8e1d832832b74790b8759f3951e0
Tarp <travistarpley0@gmail.com>
2024-10-02 17:38:39 -0700

    config: added tailwind and solid

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,10 @@ import sitemap from '@astrojs/sitemap';
 
 import tailwind from '@astrojs/tailwind';
 
+import solidJs from '@astrojs/solid-js';
+
 // https://astro.build/config
 export default defineConfig({
     site: 'https://example.com',
-    integrations: [mdx(), sitemap(), tailwind()],
+    integrations: [mdx(), sitemap(), tailwind(), solidJs()],
 });
