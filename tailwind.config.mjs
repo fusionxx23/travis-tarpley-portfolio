67d8fa3470e66ec38ec89a8bde44bad64809cb1e
Tarp <travistarpley0@gmail.com>
2024-10-03 12:47:26 -0700

    feat: Added magic ui animations

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,12 +7,23 @@ export default {
     extend: {
       keyframes: {
         fadeIn: {
-          "0%": { opacity: "0", transform: "translate(5px,5px)" },
+          "0%": { opacity: "0", transform: "translate(0px,5px)" },
           "100%": { opacity: "1", transform: "translate(0,0)" },
+        },
+        orbit: {
+          "0%": {
+            transform:
+              "rotate(0deg) translateY(calc(var(--radius) * 1px)) rotate(0deg)",
+          },
+          "100%": {
+            transform:
+              "rotate(360deg) translateY(calc(var(--radius) * 1px)) rotate(-360deg)",
+          },
         },
       },
       animation: {
         "fade-in": "fadeIn 0.5s ease-in forwards",
+        orbit: "orbit calc(var(--duration)*1s) linear infinite",
       },
       colors: {
         background: "hsl(var(--background), <alpha-value>)",
