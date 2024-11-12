e4683375e837d6ee61f9a193cc3117f5dc4b1143
Tarp <travistarpley0@gmail.com>
2024-11-12 11:34:40 -0800

    config: Added vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,11 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 import svelte from "@astrojs/svelte";
+import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   site: "https://.com",
   integrations: [mdx(), sitemap(), tailwind(), svelte()],
   output: "hybrid",
+  adapter: vercel(),
 });
