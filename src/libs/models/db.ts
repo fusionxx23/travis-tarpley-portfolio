ca1765de6725bfdccba0be440bd941e569a9003d
Tarp <travistarpley0@gmail.com>
2024-11-20 16:22:11 -0800

    fix: switched to import meta

--- src/libs/models/db.ts
+++ src/libs/models/db.ts
@@ -4,7 +4,7 @@ import { config } from "dotenv";
 config();
 export const db = drizzle({
   connection: {
-    url: process.env.TURSO_CONNECTION_URL as string,
-    authToken: process.env.TURSO_AUTH_TOKEN as string,
+    url: import.meta.env.TURSO_CONNECTION_URL as string,
+    authToken: import.meta.env.TURSO_AUTH_TOKEN as string,
   },
 });
