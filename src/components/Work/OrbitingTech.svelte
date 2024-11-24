4574e1b5db2eaa04673cb5efb6a7bb96b3f8451e
Tarp <travistarpley0@gmail.com>
2024-11-24 10:29:59 -0800

    formatting

--- src/components/Work/OrbitingTech.svelte
+++ src/components/Work/OrbitingTech.svelte
@@ -7,7 +7,7 @@
 
 <div
   class={cn(
-    "relative flex h-[400px] w-full max-w-[26rem] items-center justify-center overflow-hidden rounded-xl bg-background md:shadow-xl",
+    "relative flex h-[400px] w-full max-w-[26rem] items-center justify-center overflow-hidden rounded-xl bg-background-100 md:shadow-xl",
     className,
   )}
 >
@@ -40,7 +40,11 @@
     delay={-10}
   >
     <!-- Supabase -->
-    <svg viewBox="0 0 109 113" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg
+      viewBox="0 0 109 113"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
       <path
         d="M63.7076 110.284C60.8481 113.885 55.0502 111.912 54.9813 107.314L53.9738 40.0627L99.1935 40.0627C107.384 40.0627 111.952 49.5228 106.859 55.9374L63.7076 110.284Z"
         fill="url(#paint0_linear)"
--- src/components/Work/WorkCard.svelte
+++ src/components/Work/WorkCard.svelte
@@ -5,11 +5,13 @@
 </script>
 
 <article
-  class="flex max-w-xl flex-col items-start justify-between relative rounded-3xl p-7 border border-gray-700/70 bg-background"
+  class="flex max-w-xl flex-col items-start justify-between relative rounded-3xl p-7 border border-gray-700/70 bg-background-100"
 >
   <BorderBeam size={150} duration={12} />
   <div class="flex items-center gap-x-4 text-xs">
-    <time datetime="2020-03-16" class="text-gray-300">June 7, 2024</time>
+    <time datetime="2020-03-16" class="text-gray-300"
+      >June 7, 2024</time
+    >
     <a
       href="https://github.com/SikandarJODD"
       class="relative z-10 rounded-full bg-transparent px-3 py-1.5 font-medium text-gray-200 border border-gray-100"
@@ -25,12 +27,20 @@
         Animation Svelte
       </a>
     </h3>
-    <p class="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">
-      Welcome to Svelte Animation website. This website includes Qualtiy
-      Tailwind CSS and Framer Motion Components. Inspired from <span
-        class="text-gray-300 underline underline-offset-2">Magic UI</span
+    <p
+      class="mt-5 line-clamp-3 text-sm leading-6 text-gray-600"
+    >
+      Welcome to Svelte Animation website. This website
+      includes Qualtiy Tailwind CSS and Framer Motion
+      Components. Inspired from <span
+        class="text-gray-300 underline underline-offset-2"
+        >Magic UI</span
       >
-      and <span class="text-gray-300 underline underline-offset-2">Luxe</span>
+      and
+      <span
+        class="text-gray-300 underline underline-offset-2"
+        >Luxe</span
+      >
       Componenets.
     </p>
   </div>
