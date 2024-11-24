45231fb511ac1264b5f63eae156a682d515ab5c2
Tarp <travistarpley0@gmail.com>
2024-11-24 15:13:33 -0800

    feat: Changed site title and desc

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Travis Tarpley Portfolio";
+export const SITE_DESCRIPTION =
+  "Fullstack Developer with over 3 years of experience.";
