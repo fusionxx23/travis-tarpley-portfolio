6824de54aabafbf2c434be1bd095fd3407e0a379
Tarp <travistarpley0@gmail.com>
2024-12-03 13:45:39 -0800

    feat: Added correct site string

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,7 @@ import vercel from "@astrojs/vercel/serverless";
 import auth from "auth-astro";
 // https://astro.build/config
 export default defineConfig({
+  site: "https://travistarpley.com",
   integrations: [sitemap(), tailwind(), svelte(), auth()],
   output: "server",
   adapter: vercel({}),
