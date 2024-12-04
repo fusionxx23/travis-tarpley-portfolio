a6eabe75feccee974a7492545c596e623547f835
Tarp <travistarpley0@gmail.com>
2024-12-04 15:15:57 -0800

    feat: Create TProjectProps

---
+++ src/libs/types.ts
@@ -0,0 +1,10 @@
+export interface TProjectProps {
+  id: string;
+  description: string;
+  projectImage: ImageMetadata;
+  liveDemoUrl?: string;
+  blogPostSlug?: string;
+  youtubeModal?: string;
+  githubUrl?: string;
+  title: string;
+}
