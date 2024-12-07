4143803ef98a586c7040dee8501cdf1af2ee73d3
Travis Tarpley <34798887+fusionxx23@users.noreply.github.com>
2024-12-07 15:04:33 -0800

    feat: Added new styles to inputs

--- src/components/Blog/Updater.svelte
+++ src/components/Blog/Updater.svelte
@@ -37,70 +37,79 @@
         on:submit={handleSubmit}
       >
         <div class="flex justify-center">
-          <div class="space-y-4">
-            <input
-              type="text"
-              hidden
-              name="id"
-              value={blog.id}
-            />
+          <div class="space-y-6 max-w-[500px] mx-auto">
+            <!-- Title Input -->
             <div>
-              <div>
-                <label for="title">Title</label>
-              </div>
+              <label
+                class="block text-sm font-medium text-gray-200"
+                for="title"
+              >
+                Title
+              </label>
               <input
                 type="text"
-                class="text-black p-2"
                 name="title"
                 id="title"
-                required
+                class="mt-1 block text-black w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm p-2"
                 bind:value={title}
-              />
-            </div>
-            <div>
-              <div>
-                <label for="description">Description</label>
-              </div>
-              <input
-                type="text"
-                class="text-black p-2"
-                name="description"
-                id="description"
                 required
-                bind:value={description}
               />
             </div>
 
-            <div class="text-white">
-              <label for="file">Thumbnail</label>
+            <!-- Description Input -->
+            <div>
+              <label
+                for="description"
+                class="block text-sm font-medium text-gray-200"
+              >
+                Description
+              </label>
               <input
-                class="text-white"
+                bind:value={description}
+                type="text"
+                name="description"
+                id="description"
+                class="mt-1 block text-black w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm p-2"
+                required
+              />
+            </div>
+
+            <!-- Thumbnail File Input -->
+            <div>
+              <label
+                for="file"
+                class="block text-sm font-medium text-gray-200"
+              >
+                Thumbnail
+              </label>
+              <input
                 type="file"
                 name="file"
                 id="file"
-                accept="image/png, image/jpeg, image/webp"
+                accept="image/png, image/jpeg"
+                class="mt-1 block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-md file:border file:border-gray-300 file:text-sm file:font-medium file:bg-gray-50 file:text-gray-700 hover:file:bg-gray-100"
+                required
               />
             </div>
           </div>
-        </div>
-
-        <Editor content={blog.blogContent} />
-        <div class="flex justify-center pt-6">
-          <div>
-            <button
-              type="submit"
-              class="border group p-2 hover:bg-purple-500 disabled:hover:bg-none hover:border-black
+          <Editor content={blog.blogContent} />
+          <div class="flex justify-center pt-6">
+            <div>
+              <button
+                type="submit"
+                class="border group p-2 hover:bg-purple-500 disabled:hover:bg-none hover:border-black
           hover:text-black"
-              disabled={isLoading}
-            >
-              <div class="group-disabled:hidden">
-                Update Blog Post
-              </div>
-              <div class="group-disabled:block hidden">
-                Loading...
-              </div>
-            </button>
-            <p>{status}</p>
+                disabled={isLoading}
+              >
+                <div class="group-disabled:hidden">
+                  Update Blog Post
+                </div>
+                <div class="group-disabled:block hidden">
+                  Loading...
+                </div>
+              </button>
+              <p>{status}</p>
+            </div>
           </div>
         </div>
       </form>
