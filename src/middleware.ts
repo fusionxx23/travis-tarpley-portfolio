f545b594e570f17f74e2432ebebef733e0ba993d
Travis Tarpley <34798887+fusionxx23@users.noreply.github.com>
2024-12-07 11:17:06 -0800

    feat: Adding login middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,21 +7,26 @@ export const onRequest: MiddlewareHandler = async (
 ) => {
   const paths = context.url.pathname.split("/");
   const rootPath = paths[1];
-  const isAdminAction =
-    rootPath.toLowerCase() === "action" &&
-    paths[2].toLowerCase() === "admin";
 
-  if (rootPath.toLowerCase() === "admin" || isAdminAction) {
-    console.log("inside here.");
+  if (rootPath.toLowerCase() === "admin") {
     const session = await getSession(context.request);
     if (session?.user?.email === import.meta.env.EMAIL) {
       return next();
-    } else {
+    }
+    return new Response(null, {
+      status: 307,
+      headers: { Location: "/" },
+    });
+  }
+  if (rootPath.toLowerCase() === "login") {
+    const session = await getSession(context.request);
+    if (session?.user?.email === import.meta.env.EMAIL) {
       return new Response(null, {
         status: 307,
-        headers: { Location: "/" },
+        headers: { Location: "/admin" },
       });
     }
+    return next();
   }
   return next();
 };
