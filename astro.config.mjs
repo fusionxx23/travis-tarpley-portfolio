d9a7dd4857084c21a3e0bd9c9b02e46e7e88bfd1
Travis Tarpley <34798887+fusionxx23@users.noreply.github.com>
2024-12-08 10:28:58 -0800

    feat: Add vercel image service

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,5 +13,5 @@ export default defineConfig({
   site: "https://travis-tarpley-portfolio-4aov.vercel.app",
   integrations: [sitemap(), tailwind(), svelte(), auth()],
   output: "server",
-  adapter: vercel(),
+  adapter: vercel({ imageService: true }),
 });
